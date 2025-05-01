1e6f4a00172389f25de79a14e9bff8d0082bd625
JavPueEsc <javierpueyoescalante@gmail.com>
2025-05-01 22:00:31 +0200

    Versión 1: Diseño AutenticationActivity

---
+++ app/src/main/java/es/studium/login/AutenticacionActivity.kt
@@ -0,0 +1,12 @@
+package es.studium.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import es.studium.diagnoskin_app.R
+
+class AutenticacionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity_autenticacion)
+    }
+}
