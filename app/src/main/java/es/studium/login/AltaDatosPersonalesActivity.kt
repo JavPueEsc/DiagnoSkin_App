a300e148db526670e579579e1a8a41b833c33f4b
JavPueEsc <javierpueyoescalante@gmail.com>
2025-05-02 16:25:15 +0200

    Versión 1.2: Diseño AltaDatosPersonalesActivity

---
+++ app/src/main/java/es/studium/login/AltaDatosPersonalesActivity.kt
@@ -0,0 +1,16 @@
+package es.studium.login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import es.studium.diagnoskin_app.R
+
+class AltaDatosPersonalesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.login_activity_alta_datos_personales)
+    }
+}
