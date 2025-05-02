4b2928dff673382bac46ea84ffdac1227c475016
JavPueEsc <javierpueyoescalante@gmail.com>
2025-05-02 12:21:17 +0200

    Versión 1.1: Diseño AltaDatosDeAccesoActivity

---
+++ app/src/main/java/es/studium/login/AltaDatosDeAccesoActivity.kt
@@ -0,0 +1,15 @@
+package es.studium.login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import es.studium.diagnoskin_app.R
+
+class AltaDatosDeAccesoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity_alta_datos_de_acceso)
+    }
+}
